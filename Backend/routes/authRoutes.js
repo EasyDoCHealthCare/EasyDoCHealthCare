6f01f41be8eaa8c54cfc5c87982627b885f13a61
Debraj-Das <debrajdas.gayatri@gmail.com>
2024-01-13 18:42:47 +0530

    rearrange some files

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -15,39 +15,38 @@ router.post("/signup", async (req, res) => {
     const user = new User({ email, password });
     await user.save();
 
-    const token = jwt.sign({userId:user._id},jwtKey);
-    res.send({token:token});
+    const token = jwt.sign({ userId: user._id }, jwtKey);
+    res.send({ token: token });
 
     console.log("Completd Signup ", email, password);
   } catch (err) {
     console.log(err.message);
-     return res.status(422).json({ error: err.message });
+    return res.status(422).json({ error: err.message });
   }
-
 });
 
-
 router.post("/signin", async (req, res) => {
-   
-   const { email, password } = req.body;
-   
-   if (!email || !password) {
-      return res.status(422).json({ error: "Must provide email or password" });
-   }
-   
-   const user = await User.findOne({ email: email });
-   if (!user) {
-      return res.status(422).json({ error: "Must provide email or password" });
-   }
-   
-   try {
-      await user.comparePassword(password);
-      const token = jwt.sign({userId:user._id},jwtKey);
-      res.send({token:token});
-      console.log("Completd Signin ", email, password);
-   } catch (err) {
-      return res.status(422).json({ error: "Must provide email or password" });
-   }
-   });
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).json({ error: "Must provide email or password" });
+  }
+
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    return res.status(422).json({ error: "Must provide email or password" });
+  }
+
+  try {
+    await user.comparePassword(password);
+    console.log("here", email, password);
+
+    const token = jwt.sign({ userId: user._id }, jwtKey);
+    res.send({ token: token });
+    console.log("Completd Signin ", email, password);
+  } catch (err) {
+    return res.status(422).json({ error: "Must provide email or password" });
+  }
+});
 
 module.exports = router;
